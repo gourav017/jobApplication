1727e97474537336ca6b81756c9d4ff992e9574e
gourav017 <gouravsahu696@gmail.com>
2022-11-19 22:54:30 +0530

    implement the chakra ui in postjobpage

--- src/pages/PosttheJob.jsx
+++ src/pages/PosttheJob.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText,
-    Heading,
-    Input 
-  } from '@chakra-ui/react'
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Button,
+} from "@chakra-ui/react";
 
 const PosttheJob = () => {
   const [postThejob, setpostThejob] = useState({});
@@ -33,61 +33,63 @@ const PosttheJob = () => {
   };
 
   return (
-    <FormControl onSubmit={handleSubmit}>
-     <Heading as='h2' size='2xl'>
-       POST THE JOB
-  </Heading>
-   <FormLabel>Company name</FormLabel>
-      <Input 
+    <FormControl onSubmit={handleSubmit} w="50%" margin="auto">
+      <Heading as="h2" size="2xl">
+        POST THE JOB
+      </Heading>
+      <FormLabel>Company name</FormLabel>
+      <Input
         type="text"
         name="Company name"
         placeholder="enter company name here.."
         onChange={handleChange}
       />
-       <FormLabel>City</FormLabel>
-      <Input 
+      <FormLabel>City</FormLabel>
+      <Input
         type="text"
         name="city"
         id=""
         placeholder="enter city.."
         onChange={handleChange}
       />
-       <FormLabel>Location:</FormLabel>
-      <Input 
+      <FormLabel>Location</FormLabel>
+      <Input
         type="text"
         name="location"
         placeholder="job location.."
         onChange={handleChange}
       />
-      <select name="Role" onChange={handleChange}>
+      <Select name="Role" onChange={handleChange}>
         <option value="">Role</option>
         <option value="Frontend">Frontend</option>
         <option value="FullStack">Fullstack</option>
         <option value="Backend">Backend</option>
-      </select>
-      <select name="level" onChange={handleChange}>
+      </Select>
+      <Select name="level" onChange={handleChange}>
         <option value="">Level</option>
         <option value="Junior">Junior</option>
         <option value="Senior">Senior</option>
-      </select>
-      <Input 
+      </Select>
+      <Input
         type="text"
         name="position"
         placeholder="Position"
         onChange={handleChange}
       />
-      <Input 
+      <Input
         type="text"
         name="Language"
         placeholder="Language"
         onChange={handleChange}
       />
-      <select name="Contract" onChange={handleChange}>
+      <Select name="Contract" onChange={handleChange}>
         <option value="">Contract</option>
         <option value="full time">Full time</option>
         <option value="part time">Part time</option>
-      </select>
-      <button type="submit">submit</button>
+      </Select>
+      <Button type="submit" colorScheme="teal" size="lg">
+        Submit
+      </Button>
     </FormControl>
   );
 };
